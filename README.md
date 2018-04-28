9b1857fe48f86270626b9099ce8b48722cd59583
croft1 <mikecarter771@gmail.com>
2018-04-28 11:07:49 +1000

    update with full connected site

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-mewcinfo
+DIRECTORY FOR ROOT PAGE.
+
+/css
+/img
+/js
+/scss
+/vendor
+.gitignore
+.travis.yml
+gulpfile.js
+index.php
+LICENSE
+package.json
+package-lock.json
+README.md
---
+++ index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>mewc</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <style>
+  
+  .link{color: #e7e7e7; text-decoration: underline;}
+  .sidebar-nav  a{color: "#e7e7e7"; text-decoration: underline;}
+  .sidebar-nav{margin: 10px;}
+
+  </style>
+  
+  <body id="page-top">
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fa fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper" class="bg-secondary">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#top"><strong>Michael E W Carter</strong></a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger link" href="#">Home</a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger link" href="/blog">Blog</a>
+        </li>
+		<li>
+          <a class="js-scroll-trigger link" href="/shop">Shop</a>
+        </li>
+		<hr>
+		<li>
+          <a class="js-scroll-trigger link" href="https://bendigo-data-visualisation.herokuapp.com/">City of Bendigo - React App </a>
+        </li>
+		<li>
+          <a class="js-scroll-trigger link" href="https://portphilip-data-visualisation.herokuapp.com/">City of Port Philip - React App </a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger link" href="/monerominer">Monero Miner</a>
+        </li>
+		<li>
+          <a class="js-scroll-trigger link" href="www.musicland.com.au">MusicLand</a>
+        </li>
+		<li>
+          <a class="js-scroll-trigger link" href="/imghost">Image Hosting Tool</a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger link" href="www.hammondorgans.com.au">HammondOrgans Au</a>
+        </li>
+      </ul>
+    </nav>
+
+	<style>
+		mainheader {
+		background: linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(/img/bg.jpg)
+		}
+	</style>
+	
+    <!-- Header -->
+    <header class="mainheader masthead d-flex">
+      <div class="container text-center my-auto">
+        <h1 class="mb-1">Michael E W Carter</h1>
+        <h3 class="mb-5">
+          <em>Business, Software & Footy</em>
+        </h3>
+      </div>
+      <div class="overlay"></div>
+    </header>
+
+    <!-- About -->
+    <section class="content-section bg-light" id="about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <!--<h2>Connect!</h2>-->
+			<ul class="list-inline py-4">
+              <!--<li class="list-inline-item">-->
+                <!--<a href="#">-->
+                  <!--<i class="fa fa-facebook fa-fw fa-3x"></i>-->
+                <!--</a>-->
+              <!--</li>-->
+			  <li class="list-inline-item">
+                <a class="fa-icon" href="https://linkedin.com/in/carterm">
+                  <i class="fa fa-linkedin fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="https://twitter.com/michaelewcarter">
+                  <i class="fa fa-twitter fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a class="fa-icon" href="https://instagram.com/the_mewc">
+                  <i class="fa fa-instagram fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="http://mewc.info/blog">
+                  <i class="fa fa-book fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a href="https://www.behance.net/carterm">
+                  <i class="fa fa-behance fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a href="https://www.github.com/croft1">
+                  <i class="fa fa-github fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="http://mewc.info/shop">
+                  <i class="fa fa-shopping-bag fa-fw fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Services 
+	icons from here : 
+	http://bootstrapmaster.com/live/real-static/icons-simple-line-icons.html?
+	-->
+    <section class="content-section bg-secondary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">Projects</h3>
+          <h2 class="mb-5"></h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-map"></i>
+            </span>
+            <h4>
+              <strong>Bendigo React App</strong>
+            </h4>
+            <p class="text-faded mb-0">Fully supportive react-app to dynamically visualise</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-map"></i>
+            </span>
+            <h4>
+              <strong>Port Philip React App</strong>
+            </h4>
+            <p class="text-faded mb-0">data.gov visualisation for City of Port Philip</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-music-tone"></i>
+            </span>
+            <h4>
+              <strong>Bernies Music Land</strong>
+            </h4>
+            <p class="text-faded mb-0">Transforming an iconic businesses 8 year old website</p>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-mustache"></i>
+            </span>
+            <h4>
+              <strong>...and more </strong>
+            </h4>
+            <p class="text-faded mb-0">Open source. Android. Scripts. Deployment.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+	
+	<style>
+.portphilip {background: linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(img/portphilip.jpg);height: 400px;}
+.bendigo {background:  linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(img/bendigo.jpg);height: 400px;}
+.berniesmusicland {background:  linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(img/portphilip.jpg);}
+.hammondorgans {background:  linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(img/portphilip.jpg); height: 400px;}
+</style>
+
+    <!-- Callout -->
+    <section class="portphilip">
+      <div class="container text-center">
+        
+       
+      </div>
+    </section>
+	<section class="content-section bg-secondary" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">City of Port Philip Data Visualisation</h3>
+           <a class="btn btn-primary btn-xl" href="https://bendigo-data-visualisation.herokuapp.com/">Explore Live</a>
+        </div>
+      </div>
+    </section>
+	
+	<section class="bendigo">
+      <div class="container text-center">
+        
+     </div>
+    </section>
+	<section class="content-section  bg-secondary" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">City of Greater Bendigo Data Visualisation</h3>
+           <a class="btn btn-primary btn-xl" href="https://bendigo-data-visualisation.herokuapp.com/">Explore Live</a>
+        </div>
+      </div>
+    </section>
+	<!--
+	<section class="berniesmusicland">
+      <div class="container text-center">
+        
+     </div>
+    </section>
+	<section class="container-section  bg-primary">
+      <div class="container text-center">
+        <h2 class="mx-auto mb-5">Bernies Music Land website transformation</h2>
+        <a class="btn btn-primary btn-xl" href="https://web.archive.org/web/20170204085846/http://musicland.com.au/">Before</a>
+		<a class="btn btn-primary btn-xl" href="https://web.archive.org/web/20180227000545/http://www.musicland.com.au/">After</a>
+      </div>
+    </section>
+	
+	<section class="hammondorgans">
+      <div class="container text-center bg-primary">
+        <h2 class="mx-auto mb-5">Hammond Organs website creation</h2>
+        <a class="btn btn-primary btn-xl" href="https://bendigo-data-visualisation.herokuapp.com/">Explore Live</a>
+      </div>
+    </section>
+	-->
+	<style>.h200{height:200px;}</style>
+
+    <!-- Portfolio -->
+    <section class="content-section" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Community Involvement</h3>
+          <h2 class="mb-5"></h2>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Melbourne International Film Festival</h2>
+                  <p class="mb-0">Volunteering each year to deliver the best new films</p>
+                </span>
+              </span>
+              <img class="img-fluid" style="height: 200px" src="img/miff.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>IT/M President</h2>
+                  <p class="mb-0">IT Monash student society at Monash Caulfield</p>
+                </span>
+              </span>
+              <img class="img-fluid"  style="height: 200px" src="img/itm.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>MCC Young Members</h2>
+                  <p class="mb-0">Involved in social events and matches</p>
+                </span>
+              </span>
+              <img class="img-fluid"  style="height: 200px; margin: auto;" src="img/mccyoungmembers.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Study Abroad Australia-UK Ambassador</h2>
+                  <p class="mb-0">Representing Monash/Nottingham University</p>
+                </span>
+              </span>
+              <img class="img-fluid"  style="height: 200px" src="img/nottingham.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <ul class="list-inline py-4">
+              <!--<li class="list-inline-item">-->
+                <!--<a href="#">-->
+                  <!--<i class="fa fa-facebook fa-fw fa-3x"></i>-->
+                <!--</a>-->
+              <!--</li>-->
+			  <li class="list-inline-item">
+                <a class="fa-icon" href="https://linkedin.com/in/carterm">
+                  <i class="fa fa-linkedin fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="https://twitter.com/michaelewcarter">
+                  <i class="fa fa-twitter fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a class="fa-icon" href="https://instagram.com/the_mewc">
+                  <i class="fa fa-instagram fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="http://mewc.info/blog">
+                  <i class="fa fa-book fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a href="https://www.behance.net/carterm">
+                  <i class="fa fa-behance fa-fw fa-2x"></i>
+                </a>
+              </li>
+			  <li class="list-inline-item">
+                <a href="https://www.github.com/croft1">
+                  <i class="fa fa-github fa-fw fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="fa-icon" href="http://mewc.info/shop">
+                  <i class="fa fa-shopping-bag fa-fw fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+        <p class="text-muted small mb-0">Copyright &copy; mewc 2018</p>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.min.js"></script>
+
+  </body>
+
+</html>
